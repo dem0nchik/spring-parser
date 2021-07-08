aba98d70e9a4c3d62b61279e909dd50300fd4942
Budiuk <DBudiuk@luxoft.com>
2021-07-08 15:35:38 +0300

    added embedded tomcat in project

---
+++ src/main/java/com/test/spring/StartupApp.java
@@ -0,0 +1,16 @@
+package com.test.spring;
+
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+public class StartupApp {
+    public static void main(String[] args) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.addWebapp("", new File("src/main/").getAbsolutePath());
+        tomcat.getConnector();
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
